08fa7f0b381ec103ae52ecc7aefeef21f662e943
lz <1793607990@qq.com>
2024-09-18 23:21:54 +0800

    feat-init

---
+++ apps/next-antd/app/page.tsx
@@ -0,0 +1,11 @@
+import LayoutHeader from "@lz/layout-ui"
+
+export default function Home() {
+  return (
+    <>
+      <div className="text-red-700 bg-clip-border bg-orange-200" >12321</div>
+      <p className="border-x-4 border-indigo-200">测试header</p>
+      <LayoutHeader/>
+    </>
+  )
+}
